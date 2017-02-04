f51e49f737fdac247d61cd1eda393b7156345ca2
alexi88 <ionutz_iva@yahoo.com>
2017-02-04 13:01:28 +0200

    Added client command sender.

---
+++ WarGame/ClientCommand/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Client client = new Client();
+            //client.
+            Client.StartClient();
+            Console.ReadLine();
+
+        }
+    }
+}
