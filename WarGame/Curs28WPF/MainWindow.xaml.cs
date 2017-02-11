cf12b97fba6cc7e0cfe480047c9db47506b2e725
alexi88 <ionutz_iva@yahoo.com>
2017-02-11 13:05:07 +0200

    Added WPF Project

---
+++ WarGame/Curs28WPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Curs28WPF.ViewModels;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace Curs28WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<FarmVM> farms;
+        private FarmVM myFarm;
+
+        public ObservableCollection<FarmVM> Farms
+        {
+            get
+            {
+                return farms;
+            }
+
+            set
+            {
+                farms = value;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            Farms = new ObservableCollection<FarmVM>();
+            //FarmsLV.ItemsSource = Farms;
+            FarmsLVItemsControl.ItemsSource = Farms;
+           // myFarm = new FarmVM();
+            //Farm1.DataContext = myFarm;
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "hello world";
+        }
+
+        private void btnNewFarm_Click(object sender, RoutedEventArgs e)
+        {
+            farms.Add(new FarmVM());
+           // myFarm.X++;
+        }
+    }
+}
---
+++ WarGame/Curs28WPF/ViewModels/FarmVM.cs
@@ -0,0 +1,50 @@
+﻿using Curs28WPF.Models;
+using System.ComponentModel;
+
+namespace Curs28WPF.ViewModels
+{
+    public class FarmVM: INotifyPropertyChanged
+    {
+        private Farm farm;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public FarmVM()
+        {
+            farm = new Farm();
+        }
+
+        public FarmVM(Farm farm)
+        {
+            this.farm = farm;
+        }
+
+        public int X
+        {
+            get { return farm.X; }
+            set { farm.X = value;
+                NotifyPropertyChanged(nameof(X));
+            }
+        }
+        public int Y
+        {
+            get { return farm.Y; }
+            set { farm.Y = value;
+                NotifyPropertyChanged(nameof(Y));
+            }
+        }
+        public int Life
+        {
+            get { return farm.Life; }
+            set { farm.Life = value;
+                NotifyPropertyChanged(nameof(Life));
+
+            }
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
